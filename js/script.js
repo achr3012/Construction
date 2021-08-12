01d7705b508993c988beb80b0b2a861d92a668d4
* <achr3012@gmail.com>
2021-08-12 09:58:10 +0100

    Added About and Stats sections and fixed some bugs

--- js/script.js
+++ js/script.js
@@ -1,20 +1,32 @@
+
+
 const mainMenu = document.getElementById("mainMenu");
+
 function toggleMainMenu(x) {
+  
   x.classList.toggle("changed");
-  if (mainMenu.style.display === "block") {
+  
+  if (mainMenu.style.display === "block")
     mainMenu.style.display = "none";
-  } else {
+  else
     mainMenu.style.display = "block";
-  }
+    /* 
+    ** Here i need to implement the 
+    ** function that hides the mmen
+    ** when any othe element get clicked
+    ************************************,*/
 }
 
+
 const links = document.querySelectorAll("#mainMenu > ul > li > a");
+
 links.forEach(function(link) { 
   link.onclick = function () {
     let ulStyle = this.nextElementSibling.style;
+    
     if (ulStyle.display === "block")
       ulStyle.display = "none";
     else 
       ulStyle.display = "block";
   };
-}); 
+});
